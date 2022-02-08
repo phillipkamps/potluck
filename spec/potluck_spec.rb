db88bb9e2ab45fd3f0771e678c3350b35ab8c255
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-07 20:31:26 -0700

    Test: potluck class can accept instances of new dish class

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -4,10 +4,24 @@ require './lib/potluck'
 
 RSpec.describe Potluck do
 
-  it 'confirms class creation functions' do
+  it 'class creation functions' do
     potluck = Potluck.new('7-13-18')
 
     expect(potluck.date).to eq('7-13-18')
     expect(potluck.dishes).to eq([])
   end
+
+  it 'can accept instances of dish class' do
+    couscous_salad = Dish.new('Couscous Salad', :appetizer)
+    cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+
+    potluck = Potluck.new('7-13-18')
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+
+    expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
+    expect(potluck.dishes.length).to eq(2)
+  end
+
 end
