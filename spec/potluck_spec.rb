264d63492040800b5b9e3f68bcbd6a95881e4422
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-08 13:32:00 -0700

    Test: add 2,3 - code already passes these new tests as it is written

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Potluck do
     expect(potluck.dishes.length).to eq(2)
   end
 
-  it 'class can get info from instances of dish class' do
+  it 'class can get array of all instances of dish class which share category' do
     potluck = Potluck.new('7-13-18')
 
     couscous_salad = Dish.new('Couscous Salad', :appetizer)
@@ -41,7 +41,41 @@ RSpec.describe Potluck do
     potluck.add_dish(candy_salad)
 
     expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
-    # expect(potluck.get_all_from_category(:appetizer).first).to eq([couscous_salad])
-    # expect(potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
+  end
+
+  it 'class can get first element from array returned by get_all_from_category method' do
+    potluck = Potluck.new('7-13-18')
+
+    couscous_salad = Dish.new('Couscous Salad', :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    expect(potluck.get_all_from_category(:appetizer).first).to eq(couscous_salad)
+  end
+
+  it 'class can get name of first element from array returned by get_all_from_category method' do
+    potluck = Potluck.new('7-13-18')
+
+    couscous_salad = Dish.new('Couscous Salad', :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    expect(potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
   end
 end
